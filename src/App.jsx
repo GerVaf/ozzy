1a7de7fd08e3442c7b0fa1dcf71f041642085d9d
Wai Linn Aung <waia00198@gmail.com>
2023-10-19 17:50:44 +0630

    wailinnaung

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import FormSubmit from "./pages/FormSubmit";
+import Detail from "./pages/Detail";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/detail" element={<Detail />} />
       <Route path="/form_submit" element={<FormSubmit />} />
     </Routes>
   );
