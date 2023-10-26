29c34134deb0293a19ca7277abd9c3ddf0910d01
Wai Linn Aung <waia00198@gmail.com>
2023-10-26 13:04:28 +0630

    v7

--- src/pages/LineUp.jsx
+++ src/pages/LineUp.jsx
@@ -6,9 +6,11 @@ import img3 from "../assets/img3.jpg";
 import img4 from "../assets/img4.jpg";
 import img5 from "../assets/img5.jpg";
 import img6 from "../assets/img6.jpg";
+import { useNavigate } from "react-router-dom";
 const LineUp = () => {
+  const nav = useNavigate()
   return (
-    <div>
+    <div className="flex flex-col items-center">
       {/* Poster */}
       <div className="flex flex-col lg:flex-row lg:flex-wrap justify-center my-5 items-center gap-5">
         {/* Line up */}
@@ -20,6 +22,7 @@ const LineUp = () => {
         <img src={img5} alt="" className="w-full lg:w-[25%]" />
         <img src={img6} alt="" className="w-full lg:w-[25%]" />
       </div>
+      <button onClick={()=>nav('/')} className="bg-red-600 text-white rounded px-10 py-3 mb-5">Go Back to Home</button>
     </div>
   );
 };
